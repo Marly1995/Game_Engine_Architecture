3602e39249aeeacec2dad18516d821ef115e85a1
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2016-11-25 12:55:41 +0000

    file loads to vecots list

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
 
 #include "SDL.h"
 #include <GL/glew.h>
@@ -21,15 +22,43 @@ int main(int argc, char* argv[])
 	}
 	SDL_Log("SDL initialized OK!\n");
 
+	list<glm::vec3> positionData;
 	string line;
 	ifstream file("C:/Users/Computing/Documents/LogFile.txt");
-
+	string number;
 	if (file.is_open())
 	{
 		SDL_Log("File opened!\n");
-		
+		int dataPosition = 0;
 		while (getline(file, line))
-		{
+		{			
+			int i;
+			int k = 0;
+			float x, y, z;
+			for (i = 4; i < line.size(); i++)
+			{
+				if (line[i] != ',')
+				{
+					number += line[i];
+				}
+				else if (line[i] == ',' && k == 0)
+				{
+					x = stof(number);
+					k++;
+					i += 3;
+				}
+				else if (line[i] == ',' && k == 1)
+				{
+					y = stof(number);
+					k++;
+					i += 3;
+				}
+				else if (line[i] == ',' && k == 2)
+				{
+					z = stof(number);
+				}
+			}
+			positionData.assign(1, glm::vec3(x, y, z));
 			cout << line << "\n";
 		}
 		SDL_Log("Should have finished writing!\n");
